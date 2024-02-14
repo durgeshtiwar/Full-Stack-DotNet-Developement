76e77333d5d81c5346d11f468cbbde2282b2ddda
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-02-14 12:51:54 +0530

    First c# program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Helloworld
+{
+    class Praogram{
+        public static void Main(string[] args)
+        {
+            int x;
+            int y;
+            x = 5;
+            y = 59;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
