941d6c85ff8ff27caf6d390868380e6b3adf3426
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-02-14 17:34:40 +0530

    OddorEven

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-namespace Helloworld
-{
-    class Praogram{
+﻿// using System;
+// namespace Helloworld
+// {
+//     class Praogram{
+//         public static void Main(string[] args)
+//         {
+//             int x;
+//             int y;
+//             x = 5;
+//             y = 59;
+//             Console.WriteLine(x);
+//             Console.WriteLine(y);
+//         }
+//     }
+// }
+
+using System;
+namespace OddorEvenApp{
+    class  Praogram{
         public static void Main(string[] args)
         {
-            int x;
-            int y;
-            x = 5;
-            y = 59;
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            int num = 101;
+            if (num%2 == 0){
+                Console.WriteLine("the no is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is Odd");
+            }
         }
     }
 }
