4d82dacefb1b610d13cac1cc7ef21bccac591eca
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-02-22 21:37:43 +0530

    Stack in Generic Collection

--- Generic Collection.cs
+++ Generic Collection.cs
@@ -31,31 +31,68 @@ public class Generic_Collection
 
     //Dictionary
 
-    Dictionary <int,string> Dictionary1 = new Dictionary<int,string>();
+    // Dictionary <int,string> Dictionary1 = new Dictionary<int,string>();
+    // public void insert()
+    // {
+    //     Dictionary1.Add(1,"Durgesh Tiwari");
+    //     Dictionary1.Add(2,"B.Tech.");
+    //     Dictionary1.Add(3,"2006480100019");
+    //     Dictionary1.Add(4,"true");
+    //     Dictionary1.Add(5,"DateTime");
+    // }
+    // public void upDate()
+    // {
+    //     Dictionary1[1]="Durgesh Pratap Tiwari";
+    //     Dictionary1[4]="DateTime.Now";
+    // }
+    // public void read()
+    // {
+    //     Console.WriteLine("\nElements in the Dictionary are:");
+    //     foreach(var i in Dictionary1)
+    //     {
+    //         Console.Write("Key is "+i.Key+" and Value is "+i.Value+'\n');
+    //     }
+    // }
+    // public void delete()
+    // {
+    //     Dictionary1.Remove(4);
+    //     Dictionary1.Clear();
+    // }   
+
+
+    //Stack
+    Stack <string> stack1 = new Stack<string>();
     public void insert()
     {
-        Dictionary1.Add(1,"Durgesh Tiwari");
-        Dictionary1.Add(2,"B.Tech.");
-        Dictionary1.Add(3,"2006480100019");
-        Dictionary1.Add(4,"true");
-        Dictionary1.Add(5,"DateTime");
+        stack1.Push("Durgesh Tiwari");
+        stack1.Push("B.Tech");
+        stack1.Push("2006480100019");
+        stack1.Push("true");
+        stack1.Push("DateTime");
     }
-    public void upDate()
+    public void check()
     {
-        Dictionary1[1]="Durgesh Pratap Tiwari";
-        Dictionary1[4]="DateTime.Now";
+        if (stack1.Contains("Durgesh Pratap Tiwari"))
+        {
+            Console.WriteLine("Element exists in the Stack.\n");
+        }
+        else
+        {
+            Console.WriteLine("Element are notexists in the Stack.\n");
+        }
     }
     public void read()
     {
-        Console.WriteLine("\nElements in the Dictionary are:");
-        foreach(var i in Dictionary1)
+        Console.WriteLine("\nElements in the Stack are:");
+        foreach(var i in stack1)
         {
-            Console.Write("Key is "+i.Key+" and Value is "+i.Value+'\n');
+            Console.Write(i+"\n");
+            
         }
     }
     public void delete()
     {
-        Dictionary1.Remove(4);
-        //Dictionary1.Clear();
+        stack1.Pop();
     }   
+
 }
