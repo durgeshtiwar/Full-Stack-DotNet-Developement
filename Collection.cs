0af31726ab867ff20189af70b5811b5729f863ef
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-02-22 13:00:50 +0530

    HashTable

--- Collection.cs
+++ Collection.cs
@@ -32,30 +32,61 @@ public class Collection
 
     //Arraylist
 
-    ArrayList ArrayList1 = new ArrayList();
+    // ArrayList ArrayList1 = new ArrayList();
+    // public void insert()
+    // {
+    //     ArrayList1.Add(5);
+    //     ArrayList1.Add("Durgesh");
+    //     ArrayList1.Add(5.8);
+    //     ArrayList1.Add(true);
+    //     ArrayList1.Add("Shubham");
+    // }
+    // public void upDate()
+    // {
+    //     ArrayList1[1]="Durgesh Tiwari";
+    //     ArrayList1[4]="Sbhubham Tiwar";
+    // }
+    // public void read()
+    // {
+    //     Console.WriteLine("\nElements in the ArrayList are:");
+    //     foreach(object i in ArrayList1)
+    //     {
+    //         Console.Write(i + " ");
+    //     }
+    // }
+    // public void delete()
+    // {
+    //     ArrayList1[4]=null;
+    // }   
+
+    //HashTable
+
+
+    Hashtable HashTable1 = new Hashtable();
     public void insert()
     {
-        ArrayList1.Add(5);
-        ArrayList1.Add("Durgesh");
-        ArrayList1.Add(5.8);
-        ArrayList1.Add(true);
-        ArrayList1.Add("Shubham");
+        HashTable1.Add(1,"Durgesh Tiwari");
+        HashTable1.Add(2,"B.Tech.");
+        HashTable1.Add(3,2006480100019);
+        HashTable1.Add("isStudent",true);
+        HashTable1.Add(4,DateTime.Now);
     }
     public void upDate()
     {
-        ArrayList1[1]="Durgesh Tiwari";
-        ArrayList1[4]="Sbhubham Tiwar";
+        HashTable1[1]="Durgesh Pratap Tiwari";
+        HashTable1[4]=DateTime.Now;
     }
     public void read()
     {
-        Console.WriteLine("\nElements in the ArrayList are:");
-        foreach(object i in ArrayList1)
+        Console.WriteLine("\nElements in the HashTable are:");
+        foreach(DictionaryEntry i in HashTable1)
         {
-            Console.Write(i + " ");
+            Console.Write("Key is "+i.Key+" and Value is "+i.Value+'\n');
         }
     }
     public void delete()
     {
-        ArrayList1[4]=null;
+        HashTable1.Remove(4);
+        HashTable1.Clear();
     }   
 }
