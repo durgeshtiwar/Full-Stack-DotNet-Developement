ec4d9c3fd06d0001dc60a35f8caed71a8712a91c
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-02-25 21:41:41 +0530

    Some Changes

--- TryCatchMethod.cs
+++ TryCatchMethod.cs
@@ -6,14 +6,14 @@ public class TryCatchMethod
         try
         {
             int num1 = 10;
-            int num2 = 10;
+            int num2 = 0;
             int Result = num1/num2;
             Console.WriteLine("Resut is = "+Result);
             throw new ArgumentException("New  Exception"); // Throwing an exception from a method
         }
         catch (DivideByZeroException)
         {
-            Console.WriteLine("Divided by Zero Error Ocured");
+            Console.WriteLine("Divided by Zero Error Ocured(In MyMethod1)");
         }
         catch(Exception ex)
         {
