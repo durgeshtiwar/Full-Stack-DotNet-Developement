79c417d805419a89c17c682af76aadf441728532
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-04 11:44:44 +0530

    Web API Added

---
+++ ItExpertsEraWebApi/ItExpertsEraWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using ItExpertsEraWebApi.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ItExpertsEraWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> _employees = new List<Employee>();
+        static EmployeeController()
+        {
+            _employees.Add(new Employee() { EnployeeId = 1,EmployeeName = "Durgesh Tiawri",DepartmentName="Computer Science"});
+            _employees.Add(new Employee() { EnployeeId = 2, EmployeeName = "Aditya Singh", DepartmentName = "Information Technology" });
+            _employees.Add(new Employee() { EnployeeId = 3, EmployeeName = "Kiaslay Singh", DepartmentName = "Civil" });
+        }
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employees;
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            var employee = _employees.FirstOrDefault(Value=>Value.EnployeeId == id);
+            return employee;
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] Employee value)
+        {
+            _employees.Add(value);
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee value)
+        {
+            var employee = _employees.FirstOrDefault(item => item.EnployeeId == id);
+            employee.EmployeeName = value.EmployeeName;
+            employee.DepartmentName = value.DepartmentName;
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var employee = _employees.FirstOrDefault(item => item.EnployeeId == id);
+            _employees.Remove(employee);
+        }
+    }
+}
---
+++ ItExpertsEraWebApi/ItExpertsEraWebApi/Entity/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ItExpertsEraWebApi.Entity
+{
+    public class Employee
+    {
+        public int EnployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
