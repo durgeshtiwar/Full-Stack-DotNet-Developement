9967537ac533b26e1fbbbabaf997a4fd34243eb2
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-03-27 12:06:25 +0530

    API method

--- WebApplicationWithoutMVC/WebApplicationWithoutMVC/Program.cs
+++ WebApplicationWithoutMVC/WebApplicationWithoutMVC/Program.cs
@@ -1,12 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+//builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
+//app.Map("/Home", () => "Hello World!");
+//app.MapGet("/Home",() => "Hello this Get method");
+//app.MapPost("/Home",() => "Hello this Post method");
+//app.MapPut("/Home",() => "Hello this Put method");
+//app.MapDelete("/Home",() => "Hello this Delete method");
+
+app.UseRouting();
+app.UseEndpoints(endpoints => {
+
+    endpoints.MapGet("/Home", async (context) => 
+    {
+        await context.Response.WriteAsync("This is the Home paqe");
+    }); 
+});
+
+
+
+
+
 //app.MapControllerRoute(
 //    name:"default",
 //    pattern:"{controller=User}/{action=Index}/{id?}"
 //    );
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
--- WebApplicationWithoutMVC/WebApplicationWithoutMVC/obj/Debug/net8.0/WebApplicationWithoutMVC.AssemblyInfo.cs
+++ WebApplicationWithoutMVC/WebApplicationWithoutMVC/obj/Debug/net8.0/WebApplicationWithoutMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationWithoutMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b96ab8bee35cc3f568b18d0804466e7bc852073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7503c2c364d05fe8b1af612ffa2401805a6259d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationWithoutMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationWithoutMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
