f6a99525527a2a464b170eee7d5cb3940f3d2bed
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-07 15:46:30 +0530

    form by using tag helper

---
+++ TagHelperDemo/TagHelperDemo/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace TagHelperDemo.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string designation { get; set; }
+        public int sallary { get; set; }
+    }
+}
