747777504309ce10189e160c562f089780e92762
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-07 16:26:14 +0530

    Other form component

--- TagHelperDemo/TagHelperDemo/Models/Employee.cs
+++ TagHelperDemo/TagHelperDemo/Models/Employee.cs
@@ -4,7 +4,15 @@
     {
         public string name { get; set; }
         public int age { get; set; }
+        public Gender Gender { get; set; }
         public string designation { get; set; }
         public int sallary { get; set; }
+        public string married { get; set; }
+        public string description { get; set; }
+
+    }
+    public enum Gender
+    {
+        Male,Female
     }
 }
--- TagHelperDemo/TagHelperDemo/obj/Debug/net8.0/TagHelperDemo.AssemblyInfo.cs
+++ TagHelperDemo/TagHelperDemo/obj/Debug/net8.0/TagHelperDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TagHelperDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fcd3c2d83576d80f4112844d495f4db55f57c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a99525527a2a464b170eee7d5cb3940f3d2bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TagHelperDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TagHelperDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
