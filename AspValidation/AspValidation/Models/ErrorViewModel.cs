7f90752a729ddc0dd43f84c763e017aae7d16468
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-10 14:03:48 +0530

    Validation

---
+++ AspValidation/AspValidation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspValidation/AspValidation/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspValidation.Models
+{
+    public class Student
+    {
+        [Required (ErrorMessage ="Name must Enter")]
+        [StringLength (50,MinimumLength = 5,ErrorMessage ="Length must be between 5 and 50")]
+        public string name { get; set; }
+    }
+}
