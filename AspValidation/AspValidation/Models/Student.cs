7039db1542ccedc5caf4fda3eaaa5f84347c0e77
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-10 17:25:13 +0530

    Regular Expression

--- AspValidation/AspValidation/Models/Student.cs
+++ AspValidation/AspValidation/Models/Student.cs
@@ -4,8 +4,18 @@ namespace AspValidation.Models
 {
     public class Student
     {
-        [Required (ErrorMessage ="Name must Enter")]
+        [Required (ErrorMessage ="Name is must")]
         [StringLength (50,MinimumLength = 5,ErrorMessage ="Length must be between 5 and 50")]
         public string name { get; set; }
+        [Required(ErrorMessage ="Mail is must")]
+        //[EmailAddress] not recommended
+        [RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$",ErrorMessage ="Invalid Mail")]
+        public string email { get; set; }
+        [Required(ErrorMessage ="Age is must")]
+        [Range(21,30)]
+        public int? age { get; set; }
+        [Required(ErrorMessage ="Password is must")]
+        [RegularExpression("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$",ErrorMessage ="Upper case,Lower case,Number,min 8")]
+        public string password { get; set; }
     }
 }
