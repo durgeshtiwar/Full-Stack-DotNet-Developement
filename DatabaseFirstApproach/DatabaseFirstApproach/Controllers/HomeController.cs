1ab4b7a3c0c037368ac6fc289b88f32311729250
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-04-15 13:25:37 +0530

    DatabaseFirst Approach manually

--- DatabaseFirstApproach/DatabaseFirstApproach/Controllers/HomeController.cs
+++ DatabaseFirstApproach/DatabaseFirstApproach/Controllers/HomeController.cs
@@ -7,24 +7,97 @@ namespace DatabaseFirstApproach.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly CodeFirstDbContext std;
+        private readonly CodeFirstDbContext _contex;
 
-        public HomeController(ILogger<HomeController> logger,CodeFirstDbContext std)
+        public HomeController(ILogger<HomeController> logger,CodeFirstDbContext _contex)
         {
             _logger = logger;
-            this.std = std;
+            this._contex = _contex;
         }
 
         public IActionResult Index()
         {
-            var studentData = std.Students.ToList();
+            var studentData = _contex.Students.ToList();
             return View(studentData);
         }
-        public IActionResult Details(int id)
+        public IActionResult Details(int? id)
         {
-            var studentData = std.Students.FirstOrDefault(x => x.Id == id);
+            var studentData = _contex.Students.FirstOrDefault(x => x.Id == id);
             return View(studentData);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                var studentData = _contex.Students.Add(std);
+                _contex.SaveChanges();
+                
+            }
+            return RedirectToAction("Index", "Home");
+            //return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null||_contex.Students == null)
+            {
+                return NotFound();
+            }
+            var studentData = _contex.Students.Find(id);
+            if (studentData == null)
+            {
+                return NotFound();
+            }
+            return View(studentData);
+        }
+        [HttpPost]
+        public IActionResult Edit(int? id , Student std)
+        {
+            if (std.Id != id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var studentData = _contex.Students.Update(std);
+                _contex.SaveChanges();
+                
+            }
+            return RedirectToAction("Index", "Home");
+            //return View();
+        }
+        public IActionResult Delete(int ? id)
+        {
+            if (id == null || _contex.Students == null)
+            {
+                return NotFound();
+            }
+            var studentData = _contex.Students.FirstOrDefault(x=>x.Id == id);
+            if (studentData == null)
+            {
+                return NotFound();
+            }
+            return View(studentData);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult ConfirmDelete(int? id)
+        {
+            var studentData = _contex.Students.Find(id);
+            if (studentData != null)
+            {
+                _contex.Students.Remove(studentData);
+            }
+            _contex.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
--- DatabaseFirstApproach/DatabaseFirstApproach/obj/Debug/net8.0/DatabaseFirstApproach.AssemblyInfo.cs
+++ DatabaseFirstApproach/DatabaseFirstApproach/obj/Debug/net8.0/DatabaseFirstApproach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseFirstApproach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e94da13311b13dd47fe0494cbe9d093ac0516a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad410399967c757d5790f03274290ec8efd096df")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseFirstApproach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseFirstApproach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
